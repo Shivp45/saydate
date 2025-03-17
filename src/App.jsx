5e882a3bdaa9e73bab8cbaf75b5e636d779a3d48
Sahil Adit <aditsahil17@gmail.com>
2025-03-17 20:37:08 +0530

    nav and bg added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import Nav from "./components/nav"
+import img from './assets/bgimage.jpg';
+import mbimg from './assets/mobilebg.jpg';
 import './App.css'
 
 function App() {
 
   return (
      <div>
-      <h1 className='h'>SayDate</h1>
+      <Nav></Nav>
+      <div id='landPage'>
+        <img src={window.innerWidth <= 768 ? mbimg : img} alt="Friends" className='absolute top-0 left-0 opacity-65 -z-10 w-screen h-auto'/>
+      </div>
      </div>
   )
 }
---
+++ src/components/nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './navStyle.css';
+
+
+const Nav = () => {
+    return (
+        <div id='navbar' className='h-13 w-11/12 rounded-xl p-5 flex justify-between items-center m-auto mt-5'>
+            <div id='left'><h1 className='font-bold md:text-2xl text-xl text-white'>SayDate</h1></div>
+            <div id="right">
+                
+            </div>
+        </div>
+    );
+};
+
+
+export default Nav;
